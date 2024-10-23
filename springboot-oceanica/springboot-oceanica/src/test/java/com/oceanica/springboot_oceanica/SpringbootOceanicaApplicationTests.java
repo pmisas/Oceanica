b6cf37484aeb1a94046299304a813f104a568c60
pmisas <pmisas@unal.edu.co>
2024-10-23 14:25:44 -0500

    first commit

---
+++ springboot-oceanica/springboot-oceanica/src/test/java/com/oceanica/springboot_oceanica/SpringbootOceanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanica.springboot_oceanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOceanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

eb6714df99d7b912fb8d84fe13b276cf9b1d7017
pmisas <pmisas@unal.edu.co>
2024-10-27 16:11:41 -0500

    inicio de front con angular. creacion componente home

---
+++ angular-oceanica/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }/*,
+  { path: 'products', loadChildren: () => import('./features/products/product.module').then(m => m.ProductModule) },
+  { path: 'cart', loadChildren: () => import('./features/cart/cart.module').then(m => m.CartModule) },
+  { path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+  { path: '**', redirectTo: '' }  // Wildcard for 404 page*/
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
